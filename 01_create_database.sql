baec5df692c70c0215c3b7147b3f2ca931d7a289
Edson Damasceno <edsondamasceno22@gmail.com>
2026-02-13 19:57:09 -0300

    Add files via upload

---
+++ 01_create_database.sql
@@ -0,0 +1,13 @@
+/*
+Sales Data Analysis with SQL
+
+Este projeto utiliza o conjunto de dados "sales dataset",
+que apresenta dados de vendas sintéticos gerados para fins de simulação.
+
+O conjunto de dados pode ser acessado no Kaggle pelo link:
+https://www.kaggle.com/datasets/vinothkannaece/sales-dataset
+*/
+
+-- Criar Database
+CREATE DATABASE vendas_db;
+USE vendas_db;
